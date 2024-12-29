a02ab0a923f9fabe9e0e19f4a8bb5e34b80834ff
SafwanKS <144458370+SafwanKS@users.noreply.github.com>
2024-12-30 01:25:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express()
 const PORT = 1234
 
 app.get('/api/getName', (req, res)=>{
-  const mapHeader = req.headers[]
-  res.send(mapHeader.name);
+  const mapHeader = req.headers['']
+  console.log(mapHeader)
+  res.send('');
 })
 
 
