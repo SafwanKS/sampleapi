8b81546baf6f5ef02815f885ab8af1ad6499977e
SafwanKS <144458370+SafwanKS@users.noreply.github.com>
2024-12-30 01:53:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require("express")
 const app = express()
 const PORT = 1234
 
-app.get('/api/getName', (req, res)=>{
-  const name = req.headers['name']
-  res.json({name: name})
+app.get('/api/askAi', (req, res)=>{
+  const prompt = req.headers['prompt']
+
+const GoogleGenerativeAI = require("@google/generative-ai");
+
+const apiKey = "AIzaSyDrBOFOSO5lGJiv2CpsnysXYhxamwD8rj8"
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+const result = await model.generateText("Hey");
+const airesponse = result.text()
+  console.log(airesponse)
+  
+  res.json({response: airesponse})
 })
 
 
